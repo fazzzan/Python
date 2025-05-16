6e25496ad95d284624152f011d6ee0512bed91bc
ansible <68078825+fazzzan@users.noreply.github.com>
2025-05-16 10:19:02 +0300

    20250516

--- PycharmProjects/9_lives/stepik.py
+++ PycharmProjects/9_lives/stepik.py
@@ -1,17 +1,49 @@
-cell1 = input()
-cell2 = input()
-aceg = ['a', 'c', 'e', 'g' ]
-bdfh = ['b', 'd', 'f', 'h' ]
-if cell1.lower() == cell2.lower():
-    print("YES")
-if ((cell1[0].lower() in aceg) and (cell2[0].lower() in aceg)) or ((cell1[0].lower() in bdfh) and (cell2[0].lower() in bdfh)):
-    if int(cell1[1])%2 == int(cell2[1])%2:
-        print("YES")
-    else:
-        print("NO")
+q1 = input('Хотите много зарабатывать (Д/Н)?')
+if q1.lower() == 'н':
+    print('Delphy')
 else:
-    if int(cell1[1])%2 != int(cell2[1])%2:
-        print("YES")
+    q11 = input('Вы тупой (Д/Н)?')
+    if q11.lower() == 'н':
+        q110 = input('Вы инженер (Д/Н)?')
+        if q110.lower() == 'н':
+            q1100 = input('У вас есит ноги (Д/Н)?')
+            if q1100.lower() == 'н':
+                print('C')
+            else:
+                q11001 = input('А они вам нужны (Д/Н)?')
+                if q11001.lower() == 'н':
+                    print('C++')
+                else:
+                    print('Java')
+        else:
+            q1101 = input('Вам больше 50 (Д/Н)?')
+            if q1101.lower() == 'н':
+                print('Matlab')
+            else:
+                print('Fortran')
     else:
-        print("NO")
-
+        q111 = input('Очень (Д/Н)?')
+        if q111.lower() == 'н':
+            q1110 = input('Вы несмотрелись урокув ХАУДИ ХО (Д/Н)?')
+            if q1110.lower() == 'н':
+                q11100 = input('Вам нравится Windows (Д/Н)?')
+                if q11100.lower() == 'н':
+                    q111000 = input('Вы пи*р (Д/Н)?')
+                    if q111000.lower() == 'н':
+                        print('Perl')
+                    else:
+                        print('Swift')
+                else:
+                    print('C#')
+            else:
+                print('Python')
+        else:
+            q1111 = input('У вас есть друзья (Д/Н)?')
+            if q1111.lower() == 'н':
+                print('PHP')
+            else:
+                q11111 = input('Они тоже тупые (Д/Н)?')
+                if q11111.lower() == 'н':
+                    print('Ruby')
+                else:
+                    print('JavaScript')
